601d6016d22b276626cc75df45a67790a0a1a1ff
Semykopenko Ihor <aufrutten@gmail.com>
2022-10-23 12:29:10 +0200

    added requirements, and modify of .gitignore

---
+++ main.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def counting_unique_characters(text: str) -> int:
+    if not isinstance(text, str):
+        raise TypeError(
+            'you wrote wrong type argument')
+    counter = Counter()
+    for letter in text:
+        counter[letter] += 1
+    return len([num for num in counter.values() if num == 1])
+
+
+if __name__ == '__main__':
+    print(counting_unique_characters('abbbccdf'))
+
